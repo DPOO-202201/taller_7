fb384ab2a8379c8f249ccc87e97ddb54fe4e37bb
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-05-29 15:03:30 -0500

    Avance en testCargar()

--- src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
+++ src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
@@ -6,52 +6,63 @@ import junit.framework.TestCase;
 
 public class TestAlmacen extends TestCase {
 	
-	private Almacen almacen;
+	private Almacen almacen, testAlmacen;
 	private File archivo = new File( "./data/datos.txt" );
 	
 	public void escenario() throws AlmacenException {
 		almacen = new Almacen(archivo);
 	}
 	
-	public void testCargar() throws AlmacenException
+	public void testCargar()
 	{
+		try {
+			testAlmacen = new Almacen(archivo);
+		}
+		catch (AlmacenException e) {
+			//TODO Ver como hacer que marque error o faliure cuando reciba AlmacenException
+			//assertFalse(fName, false)
+		}
+		
 		
 	}
 	
 	
 	public void testDarCategoriaRaiz() throws AlmacenException
 	{
+		escenario();
 		Categoria testCategoriaRaiz = almacen.darCategoriaRaiz(); 
+		String testCategoriaRaizNombre = testCategoriaRaiz.darNombre();
+		assertEquals(testCategoriaRaizNombre, "Cupi2");
 	}
 	
 	public void testAgregarNodo() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testEliminarNodo() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testVenderProducto() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testBuscarNodo() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testAgregarProducto() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testEliminarProducto() throws AlmacenException
 	{
-		
+		escenario();
 	}
 	
 	public void testMetodo1() throws AlmacenException
