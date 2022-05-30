6da5522d4f6cd434123cf51d709ed5e34b865b38
JuanPRodriguezB <jp.rodriguezb20@uniandes.edu.co>
2022-05-29 21:50:36 -0500

    Mas avances en testAlmacen y creacion de .txt de pruebas

--- src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
+++ src/uniandes/cupi2/almacen/mundo/TestAlmacen.java
@@ -1,12 +1,13 @@
 package uniandes.cupi2.almacen.mundo;
 
 import java.io.File;
+import java.util.List;
 
 import junit.framework.TestCase; 
 
 public class TestAlmacen extends TestCase {
 	
-	private Almacen almacen, testAlmacen;
+	private Almacen almacen;
 	private File archivo = new File( "./data/datos.txt" );
 	
 	public void escenario() throws AlmacenException {
@@ -15,15 +16,14 @@ public class TestAlmacen extends TestCase {
 	
 	public void testCargar()
 	{
+		boolean resultado = true;
 		try {
-			testAlmacen = new Almacen(archivo);
+			almacen = new Almacen(archivo);
 		}
 		catch (AlmacenException e) {
-			//TODO Ver como hacer que marque error o faliure cuando reciba AlmacenException
-			//assertFalse(fName, false)
+			resultado = false;
 		}
-		
-		
+		assertTrue(resultado);
 	}
 	
 	
@@ -38,31 +38,75 @@ public class TestAlmacen extends TestCase {
 	public void testAgregarNodo() throws AlmacenException
 	{
 		escenario();
+		Boolean resultadoPrueba;
+		
+		String tempIdPadre = "11";
+		String tempTipo = "Categoria";
+		String tempIdentificador = "113";
+		String tempNombre = "Consolas";
+		almacen.agregarNodo(tempIdPadre, tempTipo, tempIdentificador,  tempNombre);
+		NodoAlmacen tempNodo = almacen.buscarNodo("Consolas");
+		
+		resultadoPrueba = false;
+		
+		if (tempNombre.equals(tempNodo.darNombre())) {
+			if (tempTipo.equals(tempNodo.darTipo())) {
+				if (tempIdentificador.equals(tempNodo.darIdentificador())) {
+						resultadoPrueba = true;
+				}
+			}
+		}
+		
+		assertTrue(resultadoPrueba);
 	}
 	
 	public void testEliminarNodo() throws AlmacenException
 	{
-		escenario();
+		escenario();	
+		almacen.eliminarNodo("Tecnologia");
+		NodoAlmacen tempNodo = almacen.buscarNodo("Tecnologia");
+		assertNull(tempNodo);
 	}
 	
 	public void testVenderProducto() throws AlmacenException
 	{
 		escenario();
+		almacen.venderProducto("34089951", 1);
+		NodoAlmacen tempNodo = almacen.buscarNodo("1123");
+		double tempVentas = tempNodo.darValorVentas();
+		assertNotSame(0, tempVentas);
 	}
 	
 	public void testBuscarNodo() throws AlmacenException
 	{
 		escenario();
+		NodoAlmacen tempNodo= almacen.buscarNodo("11");
+		assertNotNull(tempNodo);
 	}
 	
 	public void testAgregarProducto() throws AlmacenException
 	{
 		escenario();
+		almacen.agregarProducto("ASUS", "12345678", "PC", "QWERTYUIOP", 1000000);
+		almacen.venderProducto("12345678", 1);
+		NodoAlmacen tempNodo = almacen.buscarNodo("1121");
+		assertEquals(tempNodo.darValorVentas(),1);
 	}
 	
 	public void testEliminarProducto() throws AlmacenException
 	{
 		escenario();
+		boolean resultado = true;
+		almacen.eliminarProducto("34089951");
+		NodoAlmacen tempNodo = almacen.buscarNodo("112");
+		List<Producto> tempProductos = tempNodo.darProductos();
+		try {
+			tempProductos.get(2);
+		}
+		catch (IndexOutOfBoundsException e) {
+			resultado = false;
+		}
+		assertTrue(resultado);
 	}
 	
 	public void testMetodo1() throws AlmacenException
